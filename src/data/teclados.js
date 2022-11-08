8b5091bfd6b1aa80ead48f5a81035e38632e3c95
joaoelias1921 <joao.reis@aluno.unc.br>
2022-11-07 23:20:06 -0300

    Adição de teclados, categorias e subcategorias de jogos

---
+++ src/data/teclados.js
@@ -0,0 +1,41 @@
+const { Markup } = require("telegraf");
+
+// criando o teclado de jogos de ação
+const tecladoAcao = Markup.keyboard([
+    ["Plataforma", "Tiro/FPS", "Luta"],
+    ["Beat-em up", "Furtivo"],
+    ["Sobrevivência", "Rítmico"]
+])
+.resize();
+
+// criando o teclado de jogos de ação/aventura
+const tecladoAcaoAventura = Markup.keyboard([
+    ["Horror/Sobrevivência", "Metroidvania"],
+])
+.resize();
+
+const tecladoRPG = Markup.keyboard([
+    ["RPG de Ação", "MMORPG"],
+    ["Roguelike", "Sandbox"]
+])
+.resize();
+
+const tecladoSimulacao = Markup.keyboard([
+    ["Construção e Gestão"],
+    ["Vida", "Veículos"]
+])
+.resize();
+
+const tecladoEstrategia = Markup.keyboard([
+    ["RTS", "MOBA"]
+])
+.resize();
+
+// exportando os teclados
+module.exports = {
+    tecladoAcao,
+    tecladoAcaoAventura,
+    tecladoRPG,
+    tecladoSimulacao,
+    tecladoEstrategia
+}
--- src/index.js
+++ src/index.js
@@ -3,29 +3,77 @@ const env = require("../.env");
 
 // importando a biblioteca "Telegraf"
 const { Telegraf } = require("telegraf");
+const {
+    tecladoAcao,
+    tecladoAcaoAventura,
+    tecladoRPG,
+    tecladoSimulacao,
+    tecladoEstrategia
+} = require("./data/teclados");
 
 // Criando o objeto "bot" e o instanciando como um novo objeto da classe Telegraf
 const bot = new Telegraf(env.token);
+const mensagemTeclado = "Ótimo! Agora, escolha uma das sub-categorias:";
 
 // Iniciando o bot
 bot.start(ctx => {
     const from = ctx.update.message.from;
-    ctx.reply(`Olá! Seja bem-vindo ${from.first_name}!`);
+    if(from.id === 1351450134 || from.id === 5792995783){
+        ctx.reply(`
+            Fala ${from.first_name.split(" ")[0]}, tranquilo?
+            \nSou um bot desenvolvido para o trabalho de M2 da disciplina de Sistemas de Apoio à Decisão!
+            \nPara você, eu tenho uma ótima seleção de games da atualidade, para PCs fracos e fortes! Basta dizer qual das categorias abaixo você deseja visualizar:
+            \n1 - Ação
+            \n2 - Ação/Aventura
+            \n3 - RPG
+            \n4 - Simulação
+            \n5 - Estratégia (RTS/MOBA)
+        `);
+    }else {
+        ctx.reply("Você não tem permissão para falar comigo!")
+    }
 });
 
-// dando continuidade a conversa
-bot.on("text", async (ctx, next) => {
-    await ctx.reply("Resposta 1");
-    next();
-});
-
-bot.on("text", async (ctx, next) => {
-    await ctx.reply("Resposta 2");
-    next()
-});
-
-bot.on("text", async (ctx, next) => {
-    await ctx.reply("Resposta 3");
+// utilizando o "hears" com array de expressões
+bot.hears(["1", "2", "3", "4", "5"], ctx => {
+    // descobrindo qual numero foi digitado e exibindo o teclado correspondente
+    switch (ctx.update.message.text) {
+        case "1":
+            // exibindo o teclado, ouvindo resposta do usuário e enviando os jogos
+            ctx.reply(mensagemTeclado, tecladoAcao);
+            bot.hears([
+                "Plataforma", "Tiro/FPS", "Luta",
+                "Beat-em up", "Furtivo",
+                "Sobrevivência", "Rítmico"
+            ]);
+            break;
+        case "2":
+            ctx.reply(mensagemTeclado, tecladoAcaoAventura);
+            bot.hears([
+                "Horror/Sobrevivência", "Metroidvania",
+            ]);
+            break;
+        case "3":
+            ctx.reply(mensagemTeclado, tecladoRPG);
+            bot.hears([
+                "RPG de Ação", "MMORPG",
+                "Roguelike", "Sandbox"
+            ]);
+            break;
+        case "4":
+            ctx.reply(mensagemTeclado, tecladoSimulacao);
+            bot.hears([
+                "Construção e Gestão",
+                "Vida", "Veículos"
+            ]);
+            break;
+        case "5":
+            ctx.reply(mensagemTeclado, tecladoEstrategia);
+            bot.hears([
+                "RTS", "MOBA"
+            ]);
+            break;
+    } 
 });
 
 // Iniciando o "polling" com o servidor para verificar se há
