731e5ab6b32d0ebc8aa913fdc7ed0a84d646a626
joaoelias1921 <joao.reis@aluno.unc.br>
2022-11-08 15:36:47 -0300

    Refatoração e mudanças na estrutura, eventos de usuário

--- src/index.js
+++ src/index.js
@@ -29,22 +29,12 @@ bot.start(ctx => {
         ctx.reply(`
             Fala ${from.first_name.split(" ")[0]}, tranquilo?
             \nSou um bot desenvolvido para o trabalho de M2 da disciplina de Sistemas de Apoio à Decisão!
-            \nPara você, eu tenho uma ótima seleção de games da atualidade, para PCs fracos, medianos e atuais! Basta dizer qual das categorias abaixo você deseja visualizar:
-            \n1 - Ação
-            \n2 - Ação/Aventura
-            \n3 - RPG
-            \n4 - Simulação
-            \n5 - Estratégia (RTS/MOBA)
-            \n\nAlém de tudo isso, ainda faço as funções básicas de um chatbot, como:
-            \n\n- Repetir o que você digita
-            \n- Informar as coordenadas de uma localização que você me fornecer
-            \n- Dizer o nome e o número de um contato que você passar
-            \n- Reproduzo suas mensagens de áudio e retorno a sua duração
-            \n- Com minha trena, tiro e te informo as medidas de quaisquer fotos que você me enviar
-            \n- Se você me mandar uma figurinha, te retorno qual foi e qual o seu conjunto
+            \nSou capaz de recomendar jogos de diversas categorias para você se divertir, basta enviar a palavra "Jogar"!
+            \nAlém de tudo isso, ainda faço as funções básicas de um chatbot. Basta enviar a palavra "Outros" para ver o que eu posso fazer!
         `);
     }else {
         ctx.reply("Você não tem permissão para falar comigo!");
+        bot.stop("Usuário não autorizado.");
     }
 });
 
@@ -78,11 +68,14 @@ bot.on("voice", ctx => {ctx.reply(`Que voz linda! Seu áudio tem ${ctx.update.me
 bot.on("photo", ctx => {
     const fotos = ctx.update.message.photo;
     ctx.reply("\nLembrando que as resoluções podem variar devido a compressão do Telegram!");
+    let resolucoes = "";
     fotos.forEach((foto, i) => {
-        ctx.reply(`A resolução da ${i+1}ª foto é de:
+        resolucoes += `
+            \nA resolução da ${i+1}ª foto é de:
             \n${foto.width} x ${foto.height} pixels!
-        `);
+        `
     });
+    ctx.reply(resolucoes);
 });
 
 // tratando eventos de 'stickers'
@@ -93,103 +86,127 @@ bot.on("sticker", ctx =>{
 });
 
 // utilizando o "hears" com array de expressões
-bot.hears(["1", "2", "3", "4", "5"], ctx => {
-    // descobrindo qual numero foi digitado e exibindo o teclado correspondente
-    switch (ctx.update.message.text) {
-        case "1":
-            // exibindo subcategorias de ação, ouvindo resposta do usuário e enviando os jogos
-            ctx.reply(mensagemTeclado, tecladoAcao);
-            let recomendadosAcao = "";
-            bot.hears("Plataforma", ctx => {
-                recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.plataforma), "Plataforma"); 
-                ctx.reply(recomendadosAcao);
-            });
-            bot.hears("Tiro/FPS", ctx => {
-                recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.tiro), "Tiro"); 
-                ctx.reply(recomendadosAcao);
-            });
-            bot.hears("Luta", ctx => {
-                recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.luta), "Luta"); 
-                ctx.reply(recomendadosAcao);
-            });
-            bot.hears("Beat-em up", ctx => {
-                recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.beatemup), "Beat-em"); 
-                ctx.reply(recomendadosAcao);
-            });
-            bot.hears("Furtivo", ctx => {
-                recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.furtivo), "Stealth"); 
-                ctx.reply(recomendadosAcao);
-            });
-            bot.hears("Sobrevivência", ctx => {
-                recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.sobrevivencia), "Sobrevivência"); 
-                ctx.reply(recomendadosAcao);
-            });
-            break;
-        case "2":
-            // exibindo subcategorias de ação/aventura, ouvindo resposta do usuário e enviando os jogos
-            ctx.reply(mensagemTeclado, tecladoAcaoAventura);
-            let recomendadosAcaoAv = "";
-            bot.hears("Horror/Sobrevivência", ctx => {
-                recomendadosAcaoAv = gerarVisualizacaoJogo(jogoAleatorio(jogosAcaoAv.horror), "Horror"); 
-                ctx.reply(recomendadosAcaoAv);
-            });
-            bot.hears("Metroidvania", ctx => {
-                recomendadosAcaoAv = gerarVisualizacaoJogo(jogoAleatorio(jogosAcaoAv.metroidvania), "Metroidvania"); 
-                ctx.reply(recomendadosAcaoAv);
-            });
-            break;
-        case "3":
-            // exibindo subcategorias de RPG, ouvindo resposta do usuário e enviando os jogos
-            ctx.reply(mensagemTeclado, tecladoRPG);
-            let recomendadosRPG = "";
-            bot.hears("RPG de Ação", ctx => {
-                recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.acao), "Ação+RPG"); 
-                ctx.reply(recomendadosRPG);
-            });
-            bot.hears("MMORPG", ctx => {
-                recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.mmorpg), "MMORPG"); 
-                ctx.reply(recomendadosRPG);
-            });
-            bot.hears("Roguelike", ctx => {
-                recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.roguelike), "Roguelike"); 
-                ctx.reply(recomendadosRPG);
-            });
-            bot.hears("Sandbox", ctx => {
-                recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.sandbox), "RPG+Sandbox"); 
-                ctx.reply(recomendadosRPG);
-            });
-            break;
-        case "4":
-            // exibindo subcategorias de simulaçõa, ouvindo resposta do usuário e enviando os jogos
-            ctx.reply(mensagemTeclado, tecladoSimulacao);
-            let recomendadosSimul = "";
-            bot.hears("Construção e Gestão", ctx => {
-                recomendadosSimul = gerarVisualizacaoJogo(jogoAleatorio(jogosSimulacao.construcao), "Construção"); 
-                ctx.reply(recomendadosSimul);
-            })
-            bot.hears("Vida", ctx => {
-                recomendadosSimul = gerarVisualizacaoJogo(jogoAleatorio(jogosSimulacao.vida), "Vida"); 
-                ctx.reply(recomendadosSimul);
-            })
-            bot.hears("Veículos", ctx => {
-                recomendadosSimul = gerarVisualizacaoJogo(jogoAleatorio(jogosSimulacao.veiculos), "Veículos"); 
-                ctx.reply(recomendadosSimul);
-            })
-            break;
-        case "5":
-            // exibindo subcategorias de estratégia, ouvindo resposta do usuário e enviando os jogos
-            ctx.reply(mensagemTeclado, tecladoEstrategia);
-            let recomendadosEstrat = "";
-            bot.hears("RTS", ctx => {
-                recomendadosEstrat = gerarVisualizacaoJogo(jogoAleatorio(jogosEstrategia.rts), "RTS"); 
-                ctx.reply(recomendadosEstrat);
-            })
-            bot.hears("MOBA", ctx => {
-                recomendadosEstrat = gerarVisualizacaoJogo(jogoAleatorio(jogosEstrategia.moba), "MOBA"); 
-                ctx.reply(recomendadosEstrat);
-            })
-            break;
-    } 
+bot.hears(["Jogar", "jogar"], ctx => {
+    ctx.reply(`
+        \nPara você, eu tenho uma ótima seleção de games da atualidade, para PCs fracos, medianos e atuais! Basta dizer qual das categorias abaixo você deseja visualizar:
+        \n1 - Ação
+        \n2 - Ação/Aventura
+        \n3 - RPG
+        \n4 - Simulação
+        \n5 - Estratégia (RTS/MOBA)
+    `);
+    bot.hears(["1", "2", "3", "4", "5"], ctx => {
+        // descobrindo qual numero foi digitado e exibindo o teclado correspondente
+        switch (ctx.update.message.text) {
+            case "1":
+                // exibindo subcategorias de ação, ouvindo resposta do usuário e enviando os jogos
+                ctx.reply(mensagemTeclado, tecladoAcao);
+                let recomendadosAcao = "";
+                bot.hears("Plataforma", ctx => {
+                    recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.plataforma), "Plataforma"); 
+                    ctx.reply(recomendadosAcao);
+                });
+                bot.hears("Tiro/FPS", ctx => {
+                    recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.tiro), "Tiro"); 
+                    ctx.reply(recomendadosAcao);
+                });
+                bot.hears("Luta", ctx => {
+                    recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.luta), "Luta"); 
+                    ctx.reply(recomendadosAcao);
+                });
+                bot.hears("Beat-em up", ctx => {
+                    recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.beatemup), "Beat-em"); 
+                    ctx.reply(recomendadosAcao);
+                });
+                bot.hears("Furtivo", ctx => {
+                    recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.furtivo), "Stealth"); 
+                    ctx.reply(recomendadosAcao);
+                });
+                bot.hears("Sobrevivência", ctx => {
+                    recomendadosAcao = gerarVisualizacaoJogo(jogoAleatorio(jogosAcao.sobrevivencia), "Sobrevivência"); 
+                    ctx.reply(recomendadosAcao);
+                });
+                break;
+            case "2":
+                // exibindo subcategorias de ação/aventura, ouvindo resposta do usuário e enviando os jogos
+                ctx.reply(mensagemTeclado, tecladoAcaoAventura);
+                let recomendadosAcaoAv = "";
+                bot.hears("Horror/Sobrevivência", ctx => {
+                    recomendadosAcaoAv = gerarVisualizacaoJogo(jogoAleatorio(jogosAcaoAv.horror), "Horror"); 
+                    ctx.reply(recomendadosAcaoAv);
+                });
+                bot.hears("Metroidvania", ctx => {
+                    recomendadosAcaoAv = gerarVisualizacaoJogo(jogoAleatorio(jogosAcaoAv.metroidvania), "Metroidvania"); 
+                    ctx.reply(recomendadosAcaoAv);
+                });
+                break;
+            case "3":
+                // exibindo subcategorias de RPG, ouvindo resposta do usuário e enviando os jogos
+                ctx.reply(mensagemTeclado, tecladoRPG);
+                let recomendadosRPG = "";
+                bot.hears("RPG de Ação", ctx => {
+                    recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.acao), "Ação+RPG"); 
+                    ctx.reply(recomendadosRPG);
+                });
+                bot.hears("MMORPG", ctx => {
+                    recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.mmorpg), "MMORPG"); 
+                    ctx.reply(recomendadosRPG);
+                });
+                bot.hears("Roguelike", ctx => {
+                    recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.roguelike), "Roguelike"); 
+                    ctx.reply(recomendadosRPG);
+                });
+                bot.hears("Sandbox", ctx => {
+                    recomendadosRPG = gerarVisualizacaoJogo(jogoAleatorio(jogosRPG.sandbox), "RPG+Sandbox"); 
+                    ctx.reply(recomendadosRPG);
+                });
+                break;
+            case "4":
+                // exibindo subcategorias de simulaçõa, ouvindo resposta do usuário e enviando os jogos
+                ctx.reply(mensagemTeclado, tecladoSimulacao);
+                let recomendadosSimul = "";
+                bot.hears("Construção e Gestão", ctx => {
+                    recomendadosSimul = gerarVisualizacaoJogo(jogoAleatorio(jogosSimulacao.construcao), "Construção"); 
+                    ctx.reply(recomendadosSimul);
+                })
+                bot.hears("Vida", ctx => {
+                    recomendadosSimul = gerarVisualizacaoJogo(jogoAleatorio(jogosSimulacao.vida), "Vida"); 
+                    ctx.reply(recomendadosSimul);
+                })
+                bot.hears("Veículos", ctx => {
+                    recomendadosSimul = gerarVisualizacaoJogo(jogoAleatorio(jogosSimulacao.veiculos), "Veículos"); 
+                    ctx.reply(recomendadosSimul);
+                })
+                break;
+            case "5":
+                // exibindo subcategorias de estratégia, ouvindo resposta do usuário e enviando os jogos
+                ctx.reply(mensagemTeclado, tecladoEstrategia);
+                let recomendadosEstrat = "";
+                bot.hears("RTS", ctx => {
+                    recomendadosEstrat = gerarVisualizacaoJogo(jogoAleatorio(jogosEstrategia.rts), "RTS"); 
+                    ctx.reply(recomendadosEstrat);
+                })
+                bot.hears("MOBA", ctx => {
+                    recomendadosEstrat = gerarVisualizacaoJogo(jogoAleatorio(jogosEstrategia.moba), "MOBA"); 
+                    ctx.reply(recomendadosEstrat);
+                })
+                break;
+            default:
+                ctx.reply("Esta opção de categoria não está disponível, tente novamente!");
+        } 
+    });
+});
+
+bot.hears(["Outros", "outros"], ctx => {
+    ctx.reply(`
+        Dê uma olhada no que posso fazer:
+        \n- Repetir o que você digita
+        \n- Informar as coordenadas de uma localização que você me fornecer
+        \n- Dizer o nome e o número de um contato que você passar
+        \n- Reproduzo suas mensagens de áudio e retorno a sua duração
+        \n- Com minha trena, tiro e te informo as medidas de quaisquer fotos que você me enviar
+        \n- Se você me mandar uma figurinha, te retorno qual foi e qual seu emoji o seu conjunto
+    `);
 });
 
 // Iniciando o "polling" com o servidor para verificar se há
